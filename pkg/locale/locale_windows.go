579dbade9de7705d38828babb8755c325f2a8eba
Brycen G <brycengranville@outlook.com>
2024-11-10 09:11:49 -0500

    feat(locale): add windows support via culture settings

--- pkg/locale/locale_windows.go
+++ pkg/locale/locale_windows.go
@@ -3,8 +3,20 @@
 
 package locale
 
+import (
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
 // getWindowsLocale gets the user's locale for Windows
 func GetLocale() string {
-	// TODO: Implement this but for what benefit
+	// Exec powershell Get-Culture on Windows.
+	cmd := exec.Command("powershell", "Get-Culture | select -exp Name")
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true} // No flash bangs
+	output, err := cmd.Output()
+	if err == nil {
+		return strings.Trim(string(output), "\r\n")
+	}
 	return "en-US"
 }
